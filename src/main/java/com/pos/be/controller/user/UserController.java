e571c548b016702de33a0df7784a2610d52b30ce
Ali Raza <alirazabwp124@gmail.com>
2025-04-20 12:21:10 +0500

    added order status summary be

--- src/main/java/com/pos/be/controller/user/UserController.java
+++ src/main/java/com/pos/be/controller/user/UserController.java
@@ -4,9 +4,15 @@ import com.pos.be.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -22,6 +28,21 @@ public class UserController {
                 userService.profile(username)
         );
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<?> getCurrentUser(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("username", jwt.getClaimAsString("preferred_username"));
+
+        List<String> roles = jwt.getClaimAsStringList("roles");
+        List<String> permissions = jwt.getClaimAsStringList("permissions");
+
+        response.put("roles", roles);
+        response.put("permissions", permissions);
+
+        return ResponseEntity.ok(response);
+    }
+
 }
 
 
