9c2eb654010ffbb51aa807030a99c3d7f98a2f12
Ali Raza <alirazabwp124@gmail.com>
2025-07-26 18:36:59 +0500

    fixed product controller response

---
+++ src/main/resources/scripts/mysqluser.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS point_of_sale_db;
+DROP USER IF EXISTS `admin`@`%`;
+DROP USER IF EXISTS `operator`@`%`;
+CREATE DATABASE IF NOT EXISTS point_of_sale_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `admin`@`%` IDENTIFIED WITH mysql_native_password BY 'admin@123';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `point_of_sale_db`.* TO `admin`@`%`;
+CREATE USER IF NOT EXISTS `operator`@`%` IDENTIFIED WITH mysql_native_password BY 'operator@123';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `point_of_sale_db`.* TO `operator`@`%`;
+FLUSH PRIVILEGES;
+
+
+#
+# SHOW GRANTS FOR 'admin'@'%';
